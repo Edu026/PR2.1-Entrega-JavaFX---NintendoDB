f6599d7c457339674f181ef4754f84cb7f3ae78d
«@Edu026» <edu.amoros26@gmail.com>
2023-09-22 17:09:13 +0200

    Definir les opcions del desplegable

--- Project/src/ControllerDesktop.java
+++ Project/src/ControllerDesktop.java
@@ -3,7 +3,13 @@ import javafx.scene.control.ChoiceBox;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
-public class ControllerDesktop {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+
+public class ControllerDesktop implements Initializable
+{
 
     @FXML
     private ChoiceBox<String> choiceBox;
@@ -13,5 +19,24 @@ public class ControllerDesktop {
     
     @FXML
     private AnchorPane info;
+
+    String opcions[] = { "Personatges", "Jocs", "Consoles" };
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    // Afegeix les opcions al ChoiceBox
+    choiceBox.getItems().addAll(opcions);
+    // Selecciona la primera opció
+    choiceBox.setValue(opcions[0]);
+    // Callback que s’executa quan l’usuari escull una opció
+    choiceBox.setOnAction((event) -> { loadList(); });
+    // Carregar automàticament les dades de ‘Personatges’
+    loadList();
+    }
+    public void loadList() {
+    String opcio = choiceBox.getValue();
+    System.out.println(opcio);
+    }
+
 
 }
