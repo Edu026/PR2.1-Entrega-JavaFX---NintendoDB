f2693829511fdf158374ddf50d6c57a87d716ab9
«@Edu026» <edu.amoros26@gmail.com>
2023-09-22 17:00:59 +0200

    inici del programa (2)

--- Project/src/ControllerDesktop.java
+++ Project/src/ControllerDesktop.java
@@ -1,4 +1,17 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 public class ControllerDesktop {
 
+    @FXML
+    private ChoiceBox<String> choiceBox;
+    
+    @FXML
+    private VBox yPane;
+    
+    @FXML
+    private AnchorPane info;
 
 }
