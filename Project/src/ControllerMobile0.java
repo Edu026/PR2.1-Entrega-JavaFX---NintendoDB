03c894eacc489008a572fa9cca94aa9b526fbc5b
Edu026 <73888835+Edu026@users.noreply.github.com>
2023-10-08 23:20:12 +0200

    Falta Mobile

--- Project/src/ControllerMobile0.java
+++ Project/src/ControllerMobile0.java
@@ -1,3 +1,31 @@
-public class ControllerMobile0 {
-    
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
+public class ControllerMobile0 implements Initializable{
+
+    @FXML
+    private ListView<String> myListView;
+
+    String[] options = {"Personatges","Jocs","Consoles"};
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+        myListView.getItems().addAll(options);
+        myListView.setOnMouseClicked(event->{});
+    }
+
+   
 }
+
